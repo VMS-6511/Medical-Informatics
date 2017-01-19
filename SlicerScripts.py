2876ea4bc47f3186e92e15249c8fbd460ad599b5
Vinith Suriyakumar <suriyaku@gmail.com>
2017-01-19 00:58:29 -0500

    Update SlicerScripts.py

--- SlicerScripts.py
+++ SlicerScripts.py
@@ -1,7 +1,7 @@
 #Creates a button in the Slicer Window that prints "Button clicked!" in the console
 def createButton():
 	self.newButton = qt.QPushButton("New Button!")
-    button.show()
+    	button.show()
 	newButton.connect('clicked(bool)', onPressNewButton())
     
 def onPressNewButton():
